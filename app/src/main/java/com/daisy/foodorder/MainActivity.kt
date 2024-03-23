4a33925a99271b0d3575bf45bcc9581024e17f6d
daisy-ua <eightnum.81@gmail.com>
2024-03-23 14:02:55 +0200

    Initial commit

---
+++ app/src/main/java/com/daisy/foodorder/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.daisy.foodorder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.daisy.foodorder.ui.theme.FoodOrderTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FoodOrderTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {}
+            }
+        }
+    }
+}
+
